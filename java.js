9dcfe3508f83d1e82a81187ab04a4c8c2e9c1e9d
Joreamane <ericmfal@gmail.com>
2022-03-14 11:27:14 -0700

    Added keyboard functionality for some keys.  Next is to add shift and then the operators for keyboard functions

--- java.js
+++ java.js
@@ -9,14 +9,17 @@ input.appendChild(inputText)
 answerText.textContent = ''
 answer.appendChild(answerText)
 
-let firstNumber = ''
-let nextNumber = ''
 let activeOperator = ''
 
 let numberButtons = document.querySelectorAll('.number')
 numberButtons.forEach(numberButton => numberButton.addEventListener('click', function(e) {
   inputText.textContent = inputText.textContent + this.textContent
 }));
+window.addEventListener('keydown', function(e) {
+  const numberButtonKey = document.querySelector(`.number[data-key="${e.keyCode}"]`)
+  inputText.textContent = inputText.textContent + numberButtonKey.textContent
+})
+
 
 let operatorButtons = document.querySelectorAll('.operator')
 operatorButtons.forEach(operatorButton => operatorButton.addEventListener('click', function(e) {
